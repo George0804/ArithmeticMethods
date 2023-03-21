400f778092520c9372e98fbed04e712a6be86ada
George0804 <mamikonayn0431@gmail.com>
2023-03-22 00:46:37 +0300

    Арифметика и методы

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Арифметика
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Арифметика
+            int i = 100;
+            int b = 5;
+            double p = 5.5;
+            Console.WriteLine(p+b);
+            Console.WriteLine(i-b);
+            Console.WriteLine(i+b);
+            Console.WriteLine(i/b);
+            Console.WriteLine(i*b);
+            Console.WriteLine(i%b);
+            Console.WriteLine(i);
+            //Объединение строк методом Join
+            String a1 = "I ";
+            String a2 = "love ";
+            String a3 = "C# ";
+            String a4 = String.Join(a2,a1,a3);
+            Console.WriteLine(a4);
+            //Indexof
+            string b1 = "Hello world";
+            char ch = 'r';
+            int indexofchar = b1.IndexOf(ch);
+            Console.WriteLine(indexofchar);
+            //Split
+            string text = "кажется есть вариант получше";
+ 
+            string[] words = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+ 
+            foreach (string s in words)
+            {
+                Console.WriteLine(s);
+            }
+            //Replace
+            string tex = "Я люблю C#";
+ 
+            tex = tex.Replace("C#","Java");
+            Console.WriteLine(tex);
+            //Insert
+            String Ins = "Крутая погода";
+            String Ins2 = " cолнечная";
+            Ins = Ins.Insert(6, Ins2);
+            Console.WriteLine(Ins);
+        }   
+    }
+}
